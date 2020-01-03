23042e5f69fcf896fa1c304634f6cca9756432ef
rixo <rixo@rixo.fr>
2020-01-03 13:34:32 +0100

    wip

---
+++ cli.js
@@ -0,0 +1,4 @@
+#!/usr/bin/env node
+const esm = require('esm')
+
+esm(module)('./lib/cli')
---
+++ test/a/b/c/nested.spec.js
@@ -0,0 +1,6 @@
+import { test } from '@'
+
+test('deeply nested spec file is found', t => {
+  t.ok(true)
+  // t.ok(true)
+})
