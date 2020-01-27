9359d171f6ba12464f12b6421696581b81f72f77
rixo <rixo@rixo.fr>
2020-01-27 02:18:28 +0100

    la big migration

--- .zoarrc.js
+++ .zoarrc.js
@@ -1,28 +1,16 @@
-export default {
+module.exports = {
   // pattern(s) to find test files
-  files: "*.spec.js",
+  files: ['test/**/*.spec.js', '!test/find/sample'],
 
-  watch: {
-    // set true to make watch the default mode of zoar command
-    //
-    // use --no-watch to escape this situation
-    //
-    enabled: false,
+  // patterns of files to watch
+  watch: ['**/*.{js,css,html,json}'],
 
-    // string or string[]
-    dir: ".",
-
-    // files in watch dir(s) must match this pattern(s) to be watched
-    files: "*",
-  },
-
-  // patterns to always ignore when finding for file or watching
-  //
-  // to ignore whole directories, use patterns like this: **/node_modules
-  //
-  ignore: ["**/node_modules","**/.git"],
-
-  // force resolution of paths relative to the .rc file directory (otherwise
-  // it would be relative to the directory where you launch the zoar command)
-  cwd: __dirname,
+  // patterns to always ignore when searching for files or watching
+  ignore: [
+    '**/node_modules',
+    '**/.git',
+    './examples',
+    'test/**/sample',
+    'dist/',
+  ],
 }
--- cli.js
+++ cli.js
@@ -1,8 +1,5 @@
 #!/usr/bin/env node
 
-if (process.stdin.isTTY) {
-  require('./dist/zoar.js')
-} else {
-  const esm = require('esm')
-  esm(module)('./lib/pipe.run')
-}
+const runner = process.stdin.isTTY ? './dist/zoar.js' : './dist/pipe.run.js'
+
+require(runner)
